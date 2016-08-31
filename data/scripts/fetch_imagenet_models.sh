a6b19c61f765ed083bb833e68f99cda687bd97a4
Oh233 <macshqi@gmail.com>
2016-08-31 14:11:31 +0800

    Update download link and auto-download scripts

--- data/scripts/fetch_imagenet_models.sh
+++ data/scripts/fetch_imagenet_models.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR}
 
-URL="https://ggzola-bn1306.files.1drv.com/y3mmaGVdHr-9xkT7byW7D9sUXx3DK9JPsql80j3VgMQJ5eYtonjZaS3F2CIbaQNyM36NYeH_J8B0mxFSpaaXl-2zXxEb1K4gETBBgQYomF9k1K4R61PaeaYrqtPzpAoTPUSp-sgseEK0F0iam_icLOcrlacYIV_5gK7UwycmP87U4A/VGG16.mask.caffemodel?download&psid=1"
+URL="https://onedrive.live.com/download?resid=F371D9563727B96F!91967&authkey=!AKjrYZBFAfb6JBQ"
 
 echo "Downloading VGG16.mask.caffemodel model..."
 
@@ -12,7 +12,7 @@ wget ${URL} -O VGG16.mask.caffemodel
 
 mv VGG16.mask.caffemodel ../imagenet_models/
 
-URL="https://ggznla-bn1306.files.1drv.com/y3m-3LjmEpwi5OgbGunwG_K2JsCa4hUMReNdLfmB3IxmMDTtmDiRY2X_J_qyIKMnmHq56EBmjrqxUs4ud7OU08XqxiptxnDAMIMwK1_pEZJ_3QPOUizoP3dQa0ljR3lZ20_y0owu4jWwgvkU9EprK4BSJ4YXTLGtM1jtEBNoYdQZzg/VGG16.v2.caffemodel?download&psid=1"
+URL="https://onedrive.live.com/download?resid=F371D9563727B96F!91966&authkey=!ABoH69DkSk81FwA"
 
 echo "Downloading VGG16.v2.caffemodel model..."
 
--- data/scripts/fetch_mnc_model.sh
+++ data/scripts/fetch_mnc_model.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR}
 
-URL="https://ggzlla-bn1306.files.1drv.com/y3mNmLgTY25BYV4p7fXFSsZIeTYLcCws1VKjNJDrF7ny1Hy59LWz-RP2-SggiM0uJcllDYcgNoxMPXem6doj9IpeG7L6-H0q8j2kgbssz7nRFXmQXbXOvDgqHLmsEOGSftgFwSJI4tdgXW_-KvnVukpVRUMBThCxkE3w3OSv8PwBi0/mnc_model.caffemodel.h5?download&psid=1"
+URL="https://onedrive.live.com/download?resid=F371D9563727B96F!91968&authkey=!AGcNz7xSH5E98zg"
 
 echo "Downloading mnc model..."
 
